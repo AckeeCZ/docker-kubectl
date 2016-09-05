5a97e8ce7597168431b36f965f52a86319982b8f
Josef Gattermayer <josef.gattermayer@ackee.cz>
2016-09-05 15:16:37 +0200

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 #wait for network to initialise
 echo "sleeping"
-sleep 60
+sleep 60 && kubectl proxy --port=8081 --accept-hosts='.*' --address='0.0.0.0'
 echo "resume"
 
 exec "$@"
